3273538ca478aa4ed3511c26931be6882be7a31a
Daniele Manni <dan.manni@gmail.com>
2020-03-20 10:49:27 +0100

    [#171821415] Fix tests for QR with auxDigit equals 2

--- src/__tests__/pagopa.test.ts
+++ src/__tests__/pagopa.test.ts
@@ -182,19 +182,26 @@ describe("QrCodeFromString", () => {
       4,
       "3",
       "should succeed with valid QrCode of four digits amount"
+    ],
+    [
+      "PAGOPA|002|223456789012345678|12345678901|1234567801",
+      "234567890123456",
+      10,
+      "2",
+      "should succeed with valid QrCode for auxDigit equals 2"
     ]
   ])(
     "%s, %s, %s, %s",
     async (
       qrCodeSrt,
       paymentNoticeNumber,
-      expectedAmountLegth,
+      expectedAmountLength,
       expectedAuxDigit
     ) => {
       const validation = PaymentNoticeQrCodeFromString.decode(qrCodeSrt);
       expect(isRight(validation)).toBeTruthy();
       if (isRight(validation)) {
-        expect(validation.value.amount).toHaveLength(expectedAmountLegth);
+        expect(validation.value.amount).toHaveLength(expectedAmountLength);
         expect(validation.value.identifier).toHaveLength(6);
         expect(validation.value.version).toHaveLength(3);
         expect(validation.value.organizationFiscalCode).toHaveLength(11);
@@ -202,17 +209,35 @@ describe("QrCodeFromString", () => {
           expectedAuxDigit
         );
 
-        if (expectedAuxDigit === "0" || expectedAuxDigit === "3") {
-          // tslint:disable-next-line:no-any
-          expect((validation.value.paymentNoticeNumber as any).iuv13).toEqual(
-            paymentNoticeNumber
-          );
-        } else {
-          // tslint:disable-next-line:no-any
-          expect((validation.value.paymentNoticeNumber as any).iuv17).toEqual(
-            paymentNoticeNumber
-          );
+        switch (expectedAuxDigit) {
+          case "0":
+            expect(
+              (validation.value.paymentNoticeNumber as PaymentNoticeNumber0)
+                .iuv13
+            ).toEqual(paymentNoticeNumber);
+            break;
+          case "1":
+            expect(
+              (validation.value.paymentNoticeNumber as PaymentNoticeNumber1)
+                .iuv17
+            ).toEqual(paymentNoticeNumber);
+            break;
+          case "2":
+            expect(
+              (validation.value.paymentNoticeNumber as PaymentNoticeNumber2)
+                .iuv15
+            ).toEqual(paymentNoticeNumber);
+            break;
+          case "3":
+            expect(
+              (validation.value.paymentNoticeNumber as PaymentNoticeNumber3)
+                .iuv13
+            ).toEqual(paymentNoticeNumber);
+            break;
+          default:
+            expect(false).toBeTruthy();
         }
+
         expect(PaymentNoticeQrCodeFromString.encode(validation.value)).toEqual(
           qrCodeSrt
         );
